1ee3090b9fff67e0d6dc6e04a7889d09d829d556
YuliaGlugovskaya <124707602+YuliaGlugovskaya@users.noreply.github.com>
2023-04-13 23:56:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,7 @@ import scipy.stats as st
 chat_id = 625123880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-  pv = st.cramervonmises_2samp(x, y)[1]
-  print(pv)
+  pv = st.anderson_ksamp([x, y]).significance_level 
   if pv > 0.09:
     return False
   else:
